6baf6abacdf28982a9153690d96f976ec3d217fb
MargaritaVin <ritasergienko@rambler.ru>
2022-07-30 11:04:45 +0300

    added index.html and js

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// Task1:
+let firstName = prompt("What is your name?");
+alert(firstName);
+
+// Task2:
+let x = 8;
+let y = 5;
+
+let sum = x + y;
+let min = x - y;
+let mult = x * y;
+let div = x / y;
+let deg = x ** y;
+
+alert(sum);
+alert(min);
+alert(mult);
+alert(div);
+alert(deg);
